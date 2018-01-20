14c378cc9f72f7492d5da18da38f5b476f9d11c6
Bo <zboyang@umich.edu>
2018-01-20 07:20:42 -0500

    Added image transformation script and removed snapchat image because it caused issues. Also removed .DS_Store file from Training_Images.

--- transform_image.py
+++ transform_image.py
@@ -1,23 +1,43 @@
 # To run: python3 ..../transform_image.py <path to folder of files to transform> <path to output folder> <desired file extension, EX: '.png'> <desired width and height>
 
-from os import listdir
-from os.path import isfile, join, split
-import Image
+from PIL import Image
+import sys
+import os
+import math
 
 input_folder = sys.argv[1]
 output_folder = sys.argv[2]
 output_image_type = sys.argv[3]
-image_height = sys.argv[4] # For now, make image width same as image height
+output_image_sl = int(sys.argv[4]) # Output image side length, make all output square images for now
 
-onlyfiles = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
+onlyfiles = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
 
 for im in range(len(onlyfiles)):
 
-	image = Image.open(onlyfiles[i])
-	head, tail = os.path.split(image)
-	file_name = os.path.splitext(tail)[0]
+    img = Image.open(input_folder + onlyfiles[im])
+    file_name, file_ext = os.path.splitext(onlyfiles[im])
 
-	
-	
-	im.save(head + file_name + output_image_type)
+    width, height = img.size
+    largest_side = max(width, height)
+
+    print(width)
+    print(height)
+    # Resize if necessary
+    if (largest_side > output_image_sl):
+        scale = output_image_sl / float(largest_side)
+        new_width = int(width * scale)
+        new_height = int(height * scale) 
+        img = img.resize((new_width, new_height), Image.ANTIALIAS)
+
+    # Add whitespace if necessary
+    x1 = int(math.floor((output_image_sl - new_width) / 2))
+    y1 = int(math.floor((output_image_sl - new_height) / 2))
+
+    print(img.size)
+    enlarged_img = Image.new('RGB', (output_image_sl, output_image_sl), (255, 255, 255))
+    enlarged_img.paste(img, (x1, y1))
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    enlarged_img.save(output_folder + file_name + output_image_type)
 
