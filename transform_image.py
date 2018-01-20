b88bff2c0bc23bfd18d4c0e70e7672a93e48b210
Bo <zboyang@umich.edu>
2018-01-20 14:02:05 -0500

    started svg->png script, commented out

--- transform_image.py
+++ transform_image.py
@@ -4,6 +4,7 @@ from PIL import Image
 import sys
 import os
 import math
+import cairosvg
 
 input_folder = sys.argv[1]
 output_folder = sys.argv[2]
@@ -12,16 +13,30 @@ output_image_sl = int(sys.argv[4]) # Output image side length, make all output s
 
 onlyfiles = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
 
+count = 0
 for im in range(len(onlyfiles)):
+    if (count % 100 == 0):
+        print(str(count) + ' / ' + str(len(onlyfiles)))
+    count += 1
 
-    img = Image.open(input_folder + onlyfiles[im])
     file_name, file_ext = os.path.splitext(onlyfiles[im])
+
+"""
+    if (file_ext == '.svg'):
+        input_loc = input_folder + onlyfiles[im]
+        output_loc = output_folder + '/' + file_name + output_image_type
+        print(input_loc)
+        print(output_loc)
+        cairosvg.svg2png(input_loc, output_loc)
+    else:
+"""
+    img = Image.open(input_folder + onlyfiles[im])
 
     width, height = img.size
     largest_side = max(width, height)
 
-    print(width)
-    print(height)
+    #print(width)
+    #print(height)
     # Resize if necessary
     if (largest_side > output_image_sl):
         scale = output_image_sl / float(largest_side)
@@ -33,7 +48,7 @@ for im in range(len(onlyfiles)):
     x1 = int(math.floor((output_image_sl - new_width) / 2))
     y1 = int(math.floor((output_image_sl - new_height) / 2))
 
-    print(img.size)
+    #print(img.size)
     enlarged_img = Image.new('RGB', (output_image_sl, output_image_sl), (255, 255, 255))
     enlarged_img.paste(img, (x1, y1))
 
